4964c44be0a4c38223d22d2c1288429b8f1635b1
roman.kuznetsov <roman.kuznetsov@g5e.com>
2021-02-28 20:37:06 +0200

    Init commit. Add Jssc for reading data throw serial port

---
+++ src/main/java/Main.java
@@ -0,0 +1,29 @@
+import jssc.SerialPort;
+import jssc.SerialPortException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SerialPort serialPort = new SerialPort("COM1");
+        try {
+            //Открываем порт
+            serialPort.openPort();
+            //Выставляем параметры
+            serialPort.setParams(SerialPort.BAUDRATE_9600,
+                    SerialPort.DATABITS_8,
+                    SerialPort.STOPBITS_1,
+                    SerialPort.PARITY_NONE);
+            //Включаем аппаратное управление потоком
+            serialPort.setFlowControlMode(SerialPort.FLOWCONTROL_RTSCTS_IN |
+                    SerialPort.FLOWCONTROL_RTSCTS_OUT);
+            //Устанавливаем ивент лисенер и маску
+            serialPort.addEventListener(new PortReader(serialPort), SerialPort.MASK_RXCHAR);
+            //Отправляем запрос устройству
+            serialPort.writeString("Get data");
+        }
+        catch (SerialPortException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
