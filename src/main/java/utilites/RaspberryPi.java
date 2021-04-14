9eeb0a508900c8cb1555a5e7b66063908d06b4da
roman.kuznetsov <roman.kuznetsov@g5e.com>
2021-04-14 10:50:22 +0300

    Add shutdown method

--- src/main/java/utilites/RaspberryPi.java
+++ src/main/java/utilites/RaspberryPi.java
@@ -10,10 +10,19 @@ public class RaspberryPi {
 
     public static void reboot(){
         try {
-            Runtime r = Runtime.getRuntime();
+           final Runtime r = Runtime.getRuntime();
             r.exec("sudo reboot now");
         } catch (NumberFormatException | IOException e) {
             log.info("Restart raspberry PI failed.");
+        }
+    }
+
+    public static void shutdown(){
+        try {
+            final Runtime r = Runtime.getRuntime();
+            r.exec("sudo shutdown now");
+        } catch (NumberFormatException | IOException e) {
+            log.info("Shutdown raspberry PI failed.");
         }
     }
 
