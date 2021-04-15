59af9052568b50c425fc691fc727d4c8d6ab4c68
roman.kuznetsov <roman.kuznetsov@g5e.com>
2021-04-15 16:56:14 +0300

    Add heating state for avoid repeated logging

--- src/main/java/temperature_controller/HeatingController.java
+++ src/main/java/temperature_controller/HeatingController.java
@@ -14,25 +14,31 @@ public class HeatingController {
     @Getter
     @Setter
     private static boolean GLOBAL_STOPPED;
+    private static boolean FIRST_FLOOR_STOPPED = true;
+    private static boolean SECOND_FLOOR_STOPPED = true;
 
     public static void control(ComPortDataEntity data) {
         if (!NIGHT_MODE) {
             //first floor
-            if (data.getTempPort1() >= 30) {
+            if (data.getTempPort1() >= 30 && !FIRST_FLOOR_STOPPED) {
                 relayController.stopFirstFloorHeating();
                 HeatingControllerLogRepository.save("STOPPED 1st floor", data.getTempPort1());
-            } else if (data.getTempPort1() <= 23 && !GLOBAL_STOPPED) {
+                FIRST_FLOOR_STOPPED = true;
+            } else if (data.getTempPort1() <= 23 && !GLOBAL_STOPPED && FIRST_FLOOR_STOPPED) {
                 relayController.startFirstFloorHeating();
                 HeatingControllerLogRepository.save("started 1st floor", data.getTempPort1());
+                FIRST_FLOOR_STOPPED = false;
             }
 
             //second floor
-            if (data.getTempPort2() >= 30) {
+            if (data.getTempPort2() >= 30 && !SECOND_FLOOR_STOPPED) {
                 relayController.stopSecondFloorHeating();
                 HeatingControllerLogRepository.save("STOPPED 2nd floor", data.getTempPort2());
-            } else if (data.getTempPort2() <= 23 && !GLOBAL_STOPPED) {
+                SECOND_FLOOR_STOPPED = true;
+            } else if (data.getTempPort2() <= 23 && !GLOBAL_STOPPED && SECOND_FLOOR_STOPPED) {
                 relayController.startSecondFloorHeating();
                 HeatingControllerLogRepository.save("started 2nd floor", data.getTempPort2());
+                SECOND_FLOOR_STOPPED = false;
             }
         }
     }
