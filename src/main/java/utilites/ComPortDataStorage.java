27db6e1a8ce8609536c0c3db0ddff970bb1dff56
roman.kuznetsov <roman.kuznetsov@g5e.com>
2021-04-21 14:45:43 +0300

    save com port data every 1 hover

--- src/main/java/utilites/ComPortDataStorage.java
+++ src/main/java/utilites/ComPortDataStorage.java
@@ -15,7 +15,9 @@ public class ComPortDataStorage extends ArrayList<ComPortDataEntity> {
     public boolean add(ComPortDataEntity comportData) {
         //if(super.size() == 434) //3600 sec per hour. get new data every 9 sec.
         // for 5 minutes = 30
-        if (super.size() == 180) {
+        //for 30 min = 180
+        //for 60 min 360
+        if (super.size() == 360) {
             final ComPortDataEntity averageComPortData = ComPortDataUtility.getAverageObject(this);
             repository.save(averageComPortData);
             super.clear();
