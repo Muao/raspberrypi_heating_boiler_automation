aefd3c0203c128be0a1aa4f46d4c2f1758c19d83
roman.kuznetsov <roman.kuznetsov@g5e.com>
2021-09-06 01:25:39 +0300

    Opening serial port on a fail.

--- src/main/java/utilites/ComPortReader.java
+++ src/main/java/utilites/ComPortReader.java
@@ -3,16 +3,22 @@ package utilites;
 import DAO.entities.ComPortDataEntity;
 import com.pi4j.io.serial.*;
 import lombok.Getter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import temperature_controller.HeatingController;
 
 import java.io.IOException;
 
 public class ComPortReader implements Runnable{
+    private static final Logger log = LogManager.getLogger("ComPortReader");
     @Getter
     static private ComPortDataEntity current;
 
     public static void read() throws IOException, InterruptedException {
         final Serial serial = SerialFactory.createInstance();
+        int openingCounter = 0;
+        while (!serial.isOpen()) {
+            try {
         SerialConfig config = new SerialConfig();
         config.device(SerialPort.getDefaultPort())
                 .baud(Baud._38400)
@@ -21,7 +27,15 @@ public class ComPortReader implements Runnable{
                 .stopBits(StopBits._1)
                 .flowControl(FlowControl.NONE);
 
-        serial.open(config);
+                serial.open(config);
+            } catch (IOException e) {
+                openingCounter ++;
+                e.printStackTrace();
+                Thread.sleep(1000);
+                log.error("try to open serial port. #" + openingCounter);
+            }
+        }
+
         final ComPortDataStorage dataStorage = new ComPortDataStorage();
         serial.addListener(event -> {
             try {
